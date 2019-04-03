601677c2b5541f4de3ee00588622b5d55fa64a6f
deviprsd <deviprsd21@gmail.com>
2019-04-03 14:16:50 -0500

    better moduling

--- __main__.py
+++ __main__.py
@@ -1,7 +1,3 @@
-from .gui.application import Application, SplashScreen
-from .utilities import asset_path
+from .vaaya import run
 
-app = Application(icon=asset_path('vaaya.xbm'))
-with SplashScreen(app, asset_path('logo.jpg'), 3.0):
-    pass  # loading database and stuff
-app.mainloop()
+run()
---
+++ vaaya.py
@@ -0,0 +1,9 @@
+from .gui.application import Application, SplashScreen
+from .utilities import asset_path
+
+
+def run():
+    app = Application(icon=asset_path('vaaya.xbm'))
+    with SplashScreen(app, asset_path('logo.jpg'), 3.0):
+        pass # loading database and stuff
+    app.mainloop()
