366df4d589045497ec3d9ef439bde840e09043ec
deviprsd <deviprsd21@gmail.com>
2019-04-03 14:30:06 -0500

    fixed unstable issues

--- vaaya.py
+++ vaaya.py
@@ -6,9 +6,9 @@ from .utilities import asset_path
 
 def run():
     if sys.platform.startswith('win'):
-        app = Application(icon=asset_path('vaaya.xbm'))
-    else:
         app = Application(icon=asset_path('logo.jpg'))
+    else:
+        app = Application(icon=asset_path('vaaya.xbm'))
 
     with SplashScreen(app, asset_path('logo.jpg'), 3.0):
         pass  # loading database and stuff
